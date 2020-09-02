44f5e49df3edb1052813be98f7684965d47060f5
Kevin Daly <dalykb34@gmail.com>
2020-09-02 15:58:56 -0500

    Add project files.

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+        public BankAccount()
+        {
+        }
+        public void  Deposit(double value)
+        {
+            balance += value;
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+        public void Withdraw()
+        {
+            double withdrawlAmount;
+            bool isNumber;
+            do
+            {
+                Console.WriteLine($"Account Balance: {balance,0:c}");
+                Console.WriteLine($"How much do you want to withdraw?");
+                var userResponse = Console.ReadLine();
+                isNumber = double.TryParse(userResponse, out withdrawlAmount);
+
+                while (withdrawlAmount > balance)
+                {
+                    Console.WriteLine($"Not enough funds. Please enter an appropriate amount:");
+                    userResponse = Console.ReadLine();
+                    isNumber = double.TryParse(userResponse, out withdrawlAmount);
+                }
+
+            } while (isNumber == false);
+            balance -= withdrawlAmount;
+            Console.WriteLine($"{withdrawlAmount,0:c} was taken from your account. Current balance: {balance,0:c}");
+        }
+        
+
+    }
+
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var userData = new PersonalInformation();
+            Console.WriteLine($"What is your first name?");
+            var firstName = (Console.ReadLine());
+            
+            Console.WriteLine($"What is your last name?");
+            var lastName = (Console.ReadLine());
+
+            Console.WriteLine($"What is you Address?");
+            var address = (Console.ReadLine());
+
+            Console.WriteLine($"What city do you live in?");
+            var city = (Console.ReadLine());
+
+            Console.WriteLine($"What state do you live in?");
+            var state = (Console.ReadLine());
+
+            Console.WriteLine($"What is the zipcode?");
+            var zipCode = (Console.ReadLine());
+
+            Console.WriteLine($"What is your phone number starting with area code?");
+            userData.NumberFormatted = Console.ReadLine();
+            Console.WriteLine($"Your phone number is {userData.NumberFormatted}");
+            
+            var account = new BankAccount();
+            Console.WriteLine($"how much would you like to deposit?");
+            var amountToDeposit = double.Parse(Console.ReadLine());
+            account.Deposit(amountToDeposit);
+            Console.WriteLine($"Current balance is: ${account.GetBalance()}");
+            
+            Console.WriteLine($"How much would you like to withdraw?");
+            double.Parse(Console.ReadLine());
+            account.Withdraw( ) ;
+        }
+    }
+}
